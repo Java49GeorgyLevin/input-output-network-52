10df6d63694c847f40b5b58b16a8c2cfa5b26a84
Georgy Levin <g.phyzy@gmail.com>
2023-10-20 11:46:28 +0300

    HW 52: replace switch to invoke

--- src/telran/employees/CompanyProtocol.java
+++ src/telran/employees/CompanyProtocol.java
@@ -30,34 +30,12 @@ public class CompanyProtocol implements ApplProtocol {
 		
 		CompanyProtocol companyProtocol = this;
 		Class<? extends CompanyProtocol> clazz = companyProtocol.getClass();
-//		Method[] methods = clazz.getDeclaredMethods();		
-		String methodName = requestType.replace("/", "_");
-		
-		
-//		Method method = clazz.getDeclaredMethod(methodName, Serializable.class);
-		
+		String methodName = requestType.replace("/", "_");		
 		
 		try {
 			Method method = clazz.getDeclaredMethod(methodName, Serializable.class);
 			Serializable responseData = (Serializable) method.invoke(this, data);
 
-			
-//			Serializable responseData = switch(requestType) {
-//			case "employee/add" -> employee_add(data);
-//			case "employee/get" -> employee_get(data);
-//			case "employees/get" -> employees_get(data);
-//			case "department/update" -> department_update(data);
-//			case "employee/remove" -> employee_remove(data);
-//			case "department/salary/distribution" ->
-//			department_salary_distribution(data);
-//			case "salary/distribution" -> salary_distribution(data);
-//			case "employees/department" -> employees_department(data);
-//			case "employees/salary" -> employees_salary(data);
-//			case "employees/age" -> employees_age(data);
-//			case "salary/update" -> salary_update(data);
-//			    default -> new Response(ResponseCode.WRONG_TYPE, requestType +
-//			    		" is unsupported in the Company Protocol");
-//			};
 			response = (responseData instanceof Response) ? 
 					(Response) responseData :
 				new Response(ResponseCode.OK, responseData);
